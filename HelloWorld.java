e6ef4092106b608c7d1510d5b8bc7cd64f2eed5c
Ranjan Singh <137ranjan@gmail.com>
2022-02-10 23:07:42 +0530

    First Commit for version 1.0

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        Date date = new Date();
+        System.out.println(date);
+
+        System.out.println("Version: 1.0");
+    }
+
+}
