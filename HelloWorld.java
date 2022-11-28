9742de371044b30ae3415e8b17560c2b30986cd4
Ranjan Kumar Singh <137ranjan@gmail.com>
2022-11-28 14:07:57 +0000

    Added time measurement.

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,12 +6,16 @@ import java.util.Date;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        long startTime = System.currentTimeMillis();
 
+        System.out.println("Hello World");
         Date date = new Date();
         System.out.println(date);
-
         System.out.println("Version: 1.0");
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("Time taken to execute: " + (endTime - startTime) + " ms");
     }
 
 }
